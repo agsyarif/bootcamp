ae2b5bd83c0cb5de2ed2e37daf86889643588cd5
agsyarif <syrf553@gmail.com>
2022-09-07 12:34:57 +0700

    tambah style

--- resources/views/includes/Dashboard/style.blade.php
+++ resources/views/includes/Dashboard/style.blade.php
@@ -1,6 +1,7 @@
 {{-- <script src="https://kit.fontawesome.com/281f1a6779.js" crossorigin="anonymous"></script> --}}
 <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
+{{-- <link rel="stylesheet" href="{{ asset('css/twElement.css') }}"> --}}
 
 <link rel="stylesheet" href="{{ asset('css/regular.css') }}">
 <link rel="stylesheet" href="{{ asset('css/solid.css') }}">
@@ -10,3 +11,4 @@
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 <link rel="stylesheet" href="{{ asset('css/style_tambahan.css') }}">
 <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.2/dist/flowbite.min.css" />
+{{-- <link rel="stylesheet" href="{{ asset('css/flowbite.css') }}"> --}}
--- resources/views/includes/Landing/style.blade.php
+++ resources/views/includes/Landing/style.blade.php
@@ -1,4 +1,5 @@
-<script src="https://cdn.tailwindcss.com"></script>
+{{-- <script src="https://cdn.tailwindcss.com"></script> --}}
+<script src="{{ asset('css/tail.js') }}"></script>
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
