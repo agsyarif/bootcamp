cce9b359fdcfe5dba82c728315de3842da3a632c
agus syarif <syrf553@gmail.com>
2025-02-03 21:10:47 +0700

    add: permission seeder

---
+++ database/seeders/permissionSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class permissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = [
+            'dashboard',
+            'mentor-management',
+            'member-management',
+            'transaction',
+            'course',
+            'permission',
+            'role-management',
+            'log-activity',
+            'progress'
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate([
+                'name' => $permission,
+                'guard_name' => 'web'
+            ]);
+        }
+    }
+}
