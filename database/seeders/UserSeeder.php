7f1e8bf7c8137e954449102bfcd12ade0975250e
agus syarif <syrf553@gmail.com>
2025-02-03 21:21:55 +0700

    fix: seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,44 +17,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        // $user1 = User::create([
-        //     'name' => 'Admin',
-        //     'email' => 'userAdmin@gmail.com',
-        //     // 'user_role_id' => 1,
-        //     'skill_id' => null,
-        //     'contact_number' => null,
-        //     'description' => null,
-        //     'email_verified_at' => now(),
-        //     'password' => bcrypt('admin'),
-        //     'two_factor_secret' => null,
-        //     'two_factor_recovery_codes' => null,
-        //     'remember_token' => 'Admin',
-        //     'current_team_id' => null,
-        //     'profile_photo_path' => null,
-        //     'created_at' => now(),
-        //     'updated_at' => now()
-        // ]);
-        // $user1->assignRole('Admin');
-
-        // $user2 = User::create([
-        //     'name' => 'Mentor',
-        //     'email' => 'userMentor@gmail.com',
-        //     // 'user_role_id' => 2,
-        //     'skill_id' => null,
-        //     'contact_number' => null,
-        //     'description' => null,
-        //     'email_verified_at' => now(),
-        //     'password' => bcrypt('mentor'),
-        //     'two_factor_secret' => null,
-        //     'two_factor_recovery_codes' => null,
-        //     'remember_token' => 'Admin',
-        //     'current_team_id' => null,
-        //     'profile_photo_path' => null,
-        //     'created_at' => now(),
-        //     'updated_at' => now()
-        // ]);
-        // $user2->assignRole('Mentor');
-
         $admin = User::firstOrCreate([
             'email' => 'userAdmin@gmail.com'
         ], [
